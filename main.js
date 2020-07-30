98a55188b0c86d8400b7637cdd5ac168b0298f48
Momo Ramadori <momoramadori8@gmail.com>
2020-07-30 13:15:35 +0200

    adding graphic features

--- main.js
+++ main.js
@@ -4,44 +4,83 @@
 // La partita termina quando il giocatore becca una mina, ossia inserisce un numero "vietato", oppure se raggiunge il numero massimo possibile di numeri consentiti, ossia ha inserito tutti i numeri possibili che non sono mine!
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l'utente ha inserito un numero consentito; in altre parole, deve comunicare all'utente quante giocate ha fatto prima di perdere.
 // BONUS: all'inizio della partita, il software richiede anche un livello di difficoltà all'utente che cambia il range di numeri totali (le mine saranno sempre 16):
-// con difficoltà 0=> si gioca con numeri che vanno da 1 a 100
+// con difficoltà 0 => si gioca con numeri che vanno da 1 a 100
 // con difficoltà 1 => si gioca con numeri che vanno da 1 a 80
-// con difficoltà 2=> si gioca con numeri che vanno da 1 a 50
+// con difficoltà 2 => si gioca con numeri che vanno da 1 a 50
 
-var difficolta = prompt('Scegli un livello di difficoltà 0,1 o 2');
-//Difficoltà 0
-if (difficolta == "0") {
- var nP = 100;
-} else if (difficolta == "1") {
-    nP = 80;
-} else if (difficolta == "2") {
-    nP = 50;
-}
+
 //Creo una variabile per poter variare il numero
-var nM = 16;
+var numeroMine = 16;
+//Instanzio l'array dei numeri casuali fuori così da poterlo riutilizza lo stesso nei due click
+numeroCasuale = [];
 
-//Generare 16 numeri casuali tra 1 e 100 (le mine)
-var numeroCasuale = [];
-while (numeroCasuale.length < nM) {
-    var bomba = randomGenerator(1,nP);
-    if (numeroCasuale.includes(bomba) == false) {
-        numeroCasuale.push(bomba);
+$('button').click(function(){
+    $('.griglia').empty();
+    $('.griglia').addClass('active')
+    // Prendo la difficoltà scelta dall'utente e creo il relativo campo minato
+    var difficolta = $('select').val();
+    if (difficolta == "0") {
+        var numeriPossibili = 100;
+       } else if (difficolta == "1") {
+           numeriPossibili = 80;
+       } else if (difficolta == "2") {
+           numeriPossibili = 50;
+       }
+
+    //Generare 16 numeri casuali tra 1 e max (le mine)
+    while (numeroCasuale.length < numeroMine) {
+        var bomba = randomGenerator(1,numeriPossibili);
+        if (numeroCasuale.includes(bomba) == false) {
+            numeroCasuale.push(bomba);
+        }
     }
-}
-console.log(numeroCasuale);
+    console.log(numeroCasuale);
 
-//In seguito deve chiedere all'utente di inserire un numero alla volta, sempre compreso tra 1 e 100, che sarà la sua giocata.
-var numeriUtente = [];
-do {
-    var giocataUtente = parseInt(prompt('Inserisci un numero tra 1 e ' + nP));
-    numeriUtente.push(giocataUtente);
-} while ( numeroCasuale.includes(giocataUtente) == false && numeriUtente.length < nP - nM );
+    //creo un array con numeri da 1 a numeriPossibili
+    var rangeNumbers = [];
+    for (let index = 1; index <= numeriPossibili; index++) {
+        rangeNumbers.push(index);
+    }
+    var source   = document.getElementById("entry-template").innerHTML;
+    var template = Handlebars.compile(source);
+    for (let index = 0; index < numeriPossibili; index++) {
+        var context = {number: rangeNumbers[index]};
+        var html = template(context);
+        $('.griglia').append(html);
+    }
 
-if (numeriUtente.length == nP - nM) {
-    console.log('Complimenti, non hai preso nessuna bomba ed hai vinto!');
-} else {
-    console.log('Hai preso una bomba! il tuo punteggio è: ' + (numeriUtente.length - 1));
-}
+
+    // //In seguito deve chiedere all'utente di inserire un numero alla volta, sempre compreso tra 1 e 100, che sarà la sua giocata.
+    // var numeriUtente = [];
+    // do {
+    //     var giocataUtente = parseInt(prompt('Inserisci un numero tra 1 e ' + numeriPossibili));
+    //     numeriUtente.push(giocataUtente);
+    // } while ( numeroCasuale.includes(giocataUtente) == false && numeriUtente.length < numeriPossibili - numeroMine );
+
+    // if (numeriUtente.length == numeriPossibili - numeroMine) {
+    //     console.log('Complimenti, non hai preso nessuna bomba ed hai vinto!');
+    // } else {
+    //     console.log('Hai preso una bomba! il tuo punteggio è: ' + (numeriUtente.length - 1));
+    // }
+
+    
+})
+
+$('body').on('click','.entry', function(){
+    console.log(numeroCasuale);
+       if (numeroCasuale.includes(parseInt($(this).text()))) {
+        $(this).empty();
+        $(this).addClass('red')
+        $('.griglia').removeClass('active')
+        numeroCasuale = [];
+        alert('gioco finito, hai perso!');
+       } else {
+        $(this).empty();
+        $(this).addClass('green')
+       }
+    })
+
+
 
 function randomGenerator(min,max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
